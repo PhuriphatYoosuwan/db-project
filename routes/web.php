0d0b680a9b9908265115effe2a72105b310434ea
Phuwanata <phuwanat_ta@cmu.ac.th>
2025-10-26 00:28:31 +0700

    ed

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
+    Route::get('/users', [AdminUserController::class, 'index']);
+    Route::get('/reports', [AdminReportController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
