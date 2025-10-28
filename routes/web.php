8c34695340797ab63345e9ecb728eeb88aeddf57
PhuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2025-10-29 01:50:59 +0700

    order

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
     Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
     Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
+
+    Route::post('/cart/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
 });
 
 require __DIR__.'/auth.php';
